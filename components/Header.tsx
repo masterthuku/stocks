e098a21180eba363bb69aaeaba0f575262e2066a
peter thuku <peterthuku706@gmail.com>
2025-10-04 03:30:29 +0300

    wont

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
-import { searchStocks } from "@/lib/actions/finnhub.actions";
-import dynamic from "next/dynamic";
+import {searchStocks} from "@/lib/actions/finnhub.actions";
+import UserDropdown from "./UserDropdown";
 
-const UserDropdown = dynamic(
-  () =>
-    import("./UserDropdown").catch(() => {
-      console.warn("⚠️ UserDropdown not found, using fallback");
-      return () => <div />;
-    }),
-  { ssr: false }
-);
+
 
 const Header = async ({ user }: { user: User }) => {
-  const initialStocks = await searchStocks();
+    const initialStocks = await searchStocks();
 
-  return (
-    <header className="sticky top-0 header">
-      <div className="container header-wrapper">
-        <Link href="/">
-          <Image
-            src="/assets/icons/logo.svg"
-            alt="Signalist logo"
-            width={140}
-            height={32}
-            className="h-8 w-auto cursor-pointer"
-          />
-        </Link>
-        <nav className="hidden sm:block">
-          <NavItems initialStocks={initialStocks} />
-        </nav>
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href="/">
+                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                </Link>
+                <nav className="hidden sm:block">
+                    <NavItems initialStocks={initialStocks} />
+                </nav>
 
-        <UserDropdown user={user} initialStocks={initialStocks} />
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+                <UserDropdown user={user} initialStocks={initialStocks} />
+            </div>
+        </header>
+    )
+}
+export default Header
